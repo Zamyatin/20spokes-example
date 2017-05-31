24df64694b44bb780f642d0223ab533a3788cb65
Raj Singh <raj@foreversingh.com>
2017-05-31 13:29:34 -0500

    Simple front end validations. - validations now return bools - Figured out how to get terniary to work in view to use validations without rendering from validator - picked apart field rendering functions

--- app/assets/javascripts/components/contact.es6.jsx
+++ app/assets/javascripts/components/contact.es6.jsx
@@ -6,9 +6,12 @@ class ContactForm extends React.Component {
       firstName: '',
       lastName: '',
       email: '',
-      message: ''
+      message: '',
+      dataSubmitted: false
     }
 
+    this.validEmailFormat = this.validEmailFormat.bind(this);
+    this.validateHasContent = this.validateHasContent.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
   }
@@ -16,9 +19,8 @@ class ContactForm extends React.Component {
   displayName: "ContactForm"
 
   handleInputChange(e) {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
+    const value = e.target.value;
+    const name = e.target.name;
 
     // use some ES6 computed property syntax here:
     this.setState({
@@ -29,7 +31,7 @@ class ContactForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
 
-    let package = {
+    let data = {
       first_name: this.state.firstName,
       last_name: this.state.lastName,
       email: this.state.email,
@@ -39,67 +41,130 @@ class ContactForm extends React.Component {
     $.ajax({
       type: 'POST',
       url:'/contact',
-      data: package,
-      dataType: 'json'
+      data: data,
+      dataType: 'json',
+
+      success: () => {
+        this.setState({ dataSubmitted: true })
+      }
     })
   }
 
-  render() {
+  validateHasContent(){
+    const { firstName, lastName, email, message } = this.state;
+    return firstName && lastName && email && message;
+  }
+
+  validEmailFormat(){
+    let value = this.state.email;
+    let validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    return validEmail.test(value)
+  }
+
+  renderFirstNameField() {
+    return(
+      <div className='form-group col-sm-4'>
+        <label>First Name:</label>
+        <input
+          name='firstName'
+          type='text'
+          className='form-control'
+          value={ this.state.firstName }
+          placeholder='Ron'
+          onChange={ this.handleInputChange }>
+        </input>
+      </div>
+    )
+  }
+
+  renderLastNameField() {
+    return(
+      <div className='form-group col-sm-4'>
+        <label>Last Name:</label>
+        <input
+          name='lastName'
+          type='text'
+          className='form-control'
+          value={ this.state.lastName }
+          placeholder='Swanson'
+          onChange={ this.handleInputChange }>
+        </input>
+      </div>
+    )
+  }
+
+  renderEmailField() {
+    return(
+      <div className='form-group col-sm-4'>
+        <label>Email:</label>
+        <input
+          name='email'
+          type='text'
+          className='form-control'
+          value={ this.state.email }
+          placeholder='turkey@wrapitinbacon.com'
+          onChange={ this.handleInputChange }>
+        </input>
+        { (!this.validEmailFormat())
+          ? <div><span>Valid email format required.</span></div>
+          : null
+        }
+      </div>
+    )
+  }
+
+  renderMessageField() {
+    return(
+      <div className='form-group col-sm-12'>
+        <label>Send us a lovely message!</label>
+        <textarea
+          name='message'
+          type='text'
+          className='form-control'
+          value={ this.state.message }
+          placeholder='Government is useless.'
+          onChange={ this.handleInputChange }>
+        </textarea>
+      </div>
+    )
+  }
+
+  renderSuccessMessage() {
     return(
       <div className='container'>
         <div className='col-sm-12'>
-          <form onSubmit={ this.handleSubmit }>
-            <div className='form-group col-sm-4'>
-              <label>First Name:</label>
-              <input
-                name='firstName'
-                type='text'
-                className='form-control'
-                value={ this.state.firstName }
-                placeholder='First Name'
-                onChange={ this.handleInputChange }>
-              </input>
-            </div>
-            <div className='form-group col-sm-4'>
-              <label>Last Name:</label>
-              <input
-                name='lastName'
-                type='text'
-                className='form-control'
-                value={ this.state.lastName }
-                placeholder='Last Name'
-                onChange={ this.handleInputChange }>
-              </input>
-            </div>
-            <div className='form-group col-sm-4'>
-              <label>Email:</label>
-              <input
-                name='email'
-                type='text'
-                className='form-control'
-                value={ this.state.email }
-                placeholder='Email Address'
-                onChange={ this.handleInputChange }>
-              </input>
-            </div>
-            <div className='form-group col-sm-12'>
-              <label>Tell us something nice!</label>
-              <textarea
-                name='message'
-                type='text'
-                className='form-control'
-                value={ this.state.message }
-                placeholder='Send us your love!'
-                onChange={ this.handleInputChange }>
-              </textarea>
-            </div>
-            <div className='col-sm-4 actions'>
-              <button className='btn btn-primary'>Submit</button>
-            </div>
-          </form>
+          <span>
+            Thanks for sharing! Raj learned a whole lot this week in React just so you could do that! :D
+          </span>
         </div>
       </div>
     )
+  }
+
+  render() {
+    if (this.state.dataSubmitted === true){
+      return this.renderSuccessMessage();
+    } else {
+      return(
+        <div className='container'>
+          <div className='col-sm-12'>
+            <form onSubmit={ this.handleSubmit }>
+              { this.renderFirstNameField() }
+              { this.renderLastNameField() }
+              { this.renderEmailField() }
+              { this.renderMessageField() }
+              <div className='col-sm-4 actions'>
+                <button
+                  disabled={ !(this.validateHasContent() && this.validEmailFormat()) }
+                  className='btn btn-primary'>Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
@@ -107,5 +172,6 @@ ContactForm.propTypes = {
   firstName: React.PropTypes.string,
   lastName: React.PropTypes.string,
   email: React.PropTypes.string,
-  message: React.PropTypes.string
+  message: React.PropTypes.string,
+  dataSubmitted: React.PropTypes.bool
 };
