23f47116464385a8114d1e9e437d71b43e15b9af
Raj Singh <raj@foreversingh.com>
2017-06-05 14:40:09 -0500

    WIP: Can't seem to get any test environmen to hook up. - Tried and failed at enzyme - tried and failed with straight react-test-utils - Not sure if this is a rails/webpacker thing... need to ask.

---
+++ app/javascript/packs/tests/components/contact.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
+import App from '../../application';
+import renderer from 'react-test-renderer';
+
+test('renders an instance of the ContactForm component'), () => {
+  
+}
+
+test('renders a disabled submit button when no text input exists', () => {
+
+})
+
+test('updates the first name field updates when typed into', () => {
+
+})
+
+test('updates the last name field updates when typed into', () => {
+
+})
+
+
+test('updates the email field updates when typed into', () => {
+
+})
+
+test('updates the message field updates when typed into', () => {
+
+})
+
+test('checks for proper email format', () => {
+
+})
+
+test('calls the handleSubmit function is called when submit button is clicked'), () =>{
+
+}
